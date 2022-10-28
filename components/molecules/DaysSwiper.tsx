a2067d74102bba5ee4b55447e804cc419a497d28
Aleksandra Zofia <sawicka.aleksandra@aol.com>
2022-10-28 10:59:59 +0200

    swiper reacts to click and proper initial slide

--- components/molecules/DaysSwiper.tsx
+++ components/molecules/DaysSwiper.tsx
@@ -11,31 +11,38 @@ interface Props {
 const DaysSwiper = ({ dates }: Props) => {
   const dispatch = useDispatch()
   const currentDate = useSelector((state) => state.date.date)
-
+  const [active, setActive] = React.useState<number>(dates.indexOf(currentDate))
   return (
     <Swiper
       slidesPerView={3}
       spaceBetween={20}
       onSlideChange={(swiper) => {
         dispatch(setDate(dates[swiper.activeIndex].join()))
+        setActive(swiper.activeIndex)
       }}
-      initialSlide={dates.indexOf(currentDate)}
+      initialSlide={active}
+      centeredSlides={true}
+      centerInsufficientSlides={true}
+      centeredSlidesBounds
     >
-      {dates.map((day: string) => (
-        <SwiperSlide key={day}>
-          {/* {({isActive})=>console.log(`slide ${day[0]} is active`)} */}
-          {({ isActive }) => (
-            <div
-              className={
-                isActive
-                  ? "rounded-xl p-2 text-xs text-center bg-green text-white"
-                  : "rounded-xl p-2 text-xs text-center bg-white"
-              }
-            >
-              <p>{day[1]}</p>
-              <p>{day[0]}</p>
-            </div>
-          )}
+      {dates.map((day: Array<string>, i: number) => (
+        <SwiperSlide
+          key={day[0]}
+          onClick={() => {
+            dispatch(setDate(day.join()))
+            setActive(i)
+          }}
+        >
+          <div
+            className={
+              active === i
+                ? "rounded-xl p-2 text-xs text-center bg-green text-white"
+                : "rounded-xl p-2 text-xs text-center bg-white"
+            }
+          >
+            <p>{day[1]}</p>
+            <p>{day[0]}</p>
+          </div>
         </SwiperSlide>
       ))}
     </Swiper>
