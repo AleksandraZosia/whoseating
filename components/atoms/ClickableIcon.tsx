1e00237db7be5cbe53ea4a76e1cbee70e7c7e0e2
Aleksandra Zofia <sawicka.aleksandra@aol.com>
2022-10-30 13:13:27 +0100

    visible icons in dark mode

--- components/atoms/ClickableIcon.tsx
+++ components/atoms/ClickableIcon.tsx
@@ -8,19 +8,9 @@ interface Props {
 }
 
 const ClickableIcon = ({ handleClick, src, alt, h = 24, w = 24 }: Props) => {
-  const darkMode = () =>
-    window.matchMedia &&
-    window.matchMedia("(prefers-color-scheme: dark)").matches
-  console.log(darkMode())
   return (
     <div onClick={handleClick} className="h-fit w-fit">
-      <Image
-        src={src}
-        alt={alt}
-        height={h}
-        width={w}
-        className={`${darkMode() ? "invert" : ""}`}
-      />
+      <Image src={src} alt={alt} height={h} width={w} className="dark:invert" />
     </div>
   )
 }
--- components/molecules/TopSection.tsx
+++ components/molecules/TopSection.tsx
@@ -14,7 +14,13 @@ const TopSection = ({ title, close, back }: Props) => {
     <div className="flex w-screen justify-between px-6 pt-6 ">
       {!close ? (
         <Link href="/eating-habits/add-meals/add-meals">
-          <Image src="/back_arrow.svg" alt="go back" height={25} width={25} />
+          <Image
+            src="/back_arrow.svg"
+            alt="go back"
+            height={25}
+            width={25}
+            className="dark:invert"
+          />
         </Link>
       ) : (
         <ClickableIcon
