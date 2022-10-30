b73c8ec0e555f032d376489a767710ed74dd8dee
Aleksandra Zofia <sawicka.aleksandra@aol.com>
2022-10-30 12:45:03 +0100

    wip

--- components/atoms/ClickableIcon.tsx
+++ components/atoms/ClickableIcon.tsx
@@ -9,8 +9,14 @@ interface Props {
 
 const ClickableIcon = ({ handleClick, src, alt, h = 24, w = 24 }: Props) => {
   return (
-    <div onClick={handleClick} className="h-fit w-fit dark:text-white">
-      <Image src={src} alt={alt} height={h} width={w} />
+    <div onClick={handleClick} className="h-fit w-fit">
+      <Image
+        src={src}
+        alt={alt}
+        height={h}
+        width={w}
+        className="dark:text-white"
+      />
     </div>
   )
 }
